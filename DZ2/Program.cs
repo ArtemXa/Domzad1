49fbdb42d81052d413a7294a04dc5e073561645a
ArtemXa <artemxachik@gmail.com>
2022-09-21 23:41:43 +0300

    V2

---
+++ DZ2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число 1:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 3:");
+int c = Convert.ToInt32(Console.ReadLine());
+int max=a;
+if (b>max)max=b;
+if (c>max)max=c;
+Console.WriteLine(max);
