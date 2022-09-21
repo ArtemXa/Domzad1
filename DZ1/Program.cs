7cc50855a0e6613f5fa9e3fc23c6f35931d5cfae
ArtemXa <artemxachik@gmail.com>
2022-09-21 23:27:48 +0300

    Ver1.0

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -1,12 +1,12 @@
 ﻿Console.WriteLine("Введите число 1:");
-int numb1 = Convert.ToInt32(Console.ReadLine());
+int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число 2:");
-int numb2 = Convert.ToInt32(Console.ReadLine());
-if (numb1 > numb2)
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
 {
-    Console.WriteLine(numb1);
+    Console.WriteLine(num1);
 }
 else
 {
-    Console.WriteLine(numb2);
+    Console.WriteLine(num2);
 }
