8b068583e7e13be9acf9819fee72c3812b20dd62
ArtemXa <artemxachik@gmail.com>
2022-09-21 23:44:54 +0300

    V4

---
+++ DZ4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число :");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = 1;
+int i = m;
+while (i <= n)
+{
+    i++;
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+}
