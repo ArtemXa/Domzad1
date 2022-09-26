295c9d29d4a77c7b0be9af901930bc4c4a99e170
ArtemXa <112820278+ArtemXa@users.noreply.github.com>
2022-09-26 10:50:25 +0300

    Update Program.cs

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -1,10 +1,17 @@
-﻿Console.WriteLine("Введите число 1:");
+Console.WriteLine("Введите число 1:");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число 2:");
 int b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число 3:");
 int c = Convert.ToInt32(Console.ReadLine());
-int max=a;
-if (b>max)max=b;
-if (c>max)max=c;
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+
 Console.WriteLine(max);
